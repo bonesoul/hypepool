8aaf4d199e9559b930e3350da52a5a578b432353
huseyin uslu <shalafiraistlin@gmail.com>
2018-01-11 21:43:49 +0300

    nupkg cooking done.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,17 +10,30 @@ module.exports = function (grunt) {
   //require('time-grunt')(grunt);
 
   grunt.initConfig({
+    vcpkg_dir: 'build\\vcpkg', // vcpkg dir.
+    deps_dir: 'deps\\windows', // deps dir.
+    vcpkg: 'build\\vcpkg\\vcpkg', // vcpkg executable.
     shell: {
       clone_vcpkg: {
-        command: 'git clone https://github.com/Microsoft/vcpkg.git build/vcpkg',
+        command: 'git clone https://github.com/Microsoft/vcpkg.git <%= vcpkg_dir %>',
         options: {
           failOnError: false
         }
       },
-      install_vcpkg: 'build\\vcpkg\\bootstrap-vcpkg.bat',
-      install_packages: 'build\\vcpkg\\vcpkg install boost-system libsodium',
-      cook_libcryptonote_deps: 'build\\vcpkg\\vcpkg export --nuget --nuget-id=libcryptonote.x86.deps --nuget-version=0.0.1 boost-system',
-      cook_libmultihash_deps: 'build\\vcpkg\\vcpkg export --nuget --nuget-id=libmultihash.x86.deps --nuget-version=0.0.1 libsodium',
+      install_vcpkg: '<%= vcpkg_dir %>\\bootstrap-vcpkg.bat',
+      install_packages: '<%= vcpkg %> install boost-system libsodium',
+      cook_libcryptonote_deps: '<%= vcpkg %> export --nuget --nuget-id=libcryptonote.x86.deps --nuget-version=0.0.1 boost-system',
+      cook_libmultihash_deps: '<%= vcpkg %> export --nuget --nuget-id=libmultihash.x86.deps --nuget-version=0.0.1 libsodium',
+    },
+    copy: {
+     packages: {
+       files: [{
+         expand: true,
+         cwd: '<%= vcpkg_dir %>',
+         src: ['*.nupkg'],
+         dest: '<%= deps_dir %>'
+       }]
+     }
     }
   });
 
@@ -30,10 +43,10 @@ module.exports = function (grunt) {
   // task steps.
   grunt.registerTask('vcpkg', ['shell:clone_vcpkg', 'shell:install_vcpkg']);
   grunt.registerTask('cook_windows_deps', ['shell:install_packages', 'shell:cook_libcryptonote_deps', 'shell:cook_libmultihash_deps']);
-  grunt.registerTask('copy_windows_deps', []);
+  grunt.registerTask('copy_windows_deps', ['copy:packages']);
 
   // build tasks.
-  grunt.registerTask('windows-deps', ['vcpkg', 'cook_windows_deps', 'copy_windows_deps']);
+  grunt.registerTask('cook-windows-deps', ['vcpkg', 'cook_windows_deps', 'copy_windows_deps']);
   grunt.registerTask('build', []);
 
   // default task.

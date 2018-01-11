cd976abc64b6591937bba65ed9c1902e9e1b3782
huseyin uslu <shalafiraistlin@gmail.com>
2018-01-11 18:05:52 +0300

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,28 +11,13 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     shell: {
-      clone_vcpkg: {
-        command: 'git clone https://github.com/Microsoft/vcpkg.git build/vcpkg',
-        options: {
-          failOnError: false
-        }
-      },
-      install_vcpkg: 'build\\vcpkg\\bootstrap-vcpkg.bat',
-      install_packages: 'build\\vcpkg\\vcpkg install boost-system libsodium',
-      cook_libcryptonote_deps: 'build\\vcpkg\\vcpkg export --nuget --nuget-id=libcryptonote.x86.deps --nuget-version=0.0.1 boost-system',
-      cook_libmultihash_deps: 'build\\vcpkg\\vcpkg export --nuget --nuget-id=libmultihash.x86.deps --nuget-version=0.0.1 libsodium',
-    }
   });
 
   // load all plugins.
   require('load-grunt-tasks')(grunt);
 
   // task steps.
-  grunt.registerTask('vcpkg', ['shell:clone_vcpkg', 'shell:install_vcpkg']);
-  grunt.registerTask('cook_dependencies', ['shell:install_packages', 'shell:cook_libcryptonote_deps', 'shell:cook_libmultihash_deps']);
-
   // build tasks.
-  grunt.registerTask('deps', ['vcpkg', 'cook_dependencies']);
   grunt.registerTask('build', []);
 
   // default task.

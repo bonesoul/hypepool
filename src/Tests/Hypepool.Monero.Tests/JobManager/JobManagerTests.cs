8d54176a77c07b570e6e8df3fe4a766002dbe5cd
huseyin uslu <shalafiraistlin@gmail.com>
2018-02-01 14:16:16 +0300

    test fix.

--- src/Tests/Hypepool.Monero.Tests/JobManager/JobManagerTests.cs
+++ src/Tests/Hypepool.Monero.Tests/JobManager/JobManagerTests.cs
@@ -31,6 +31,7 @@ using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Hypepool.Common.Daemon;
+using Hypepool.Common.Mining.Jobs;
 using Hypepool.Common.Pools;
 using Hypepool.Monero.Daemon.Requests;
 using Hypepool.Monero.Daemon.Responses;
@@ -44,18 +45,18 @@ namespace Hypepool.Monero.Tests.JobManager
     {
         private readonly MoneroPoolContext _poolContext;
         private readonly IDaemonClient _daemon;
-
+        private readonly MoneroJobManager _jobManager;
 
         public JobManagerTests()
         {
             // mock objects.
             _daemon = Substitute.For<IDaemonClient>();
 
-            var jobManager = new MoneroJobManager();
+            _jobManager = new MoneroJobManager();
             _poolContext = new MoneroPoolContext();
 
-            _poolContext.Configure(_daemon, _daemon, jobManager, null);
-            jobManager.Configure(_poolContext);
+            _poolContext.Configure(_daemon, _daemon, _jobManager, null);
+            _jobManager.Configure(_poolContext);
         }
 
         [Fact]
@@ -75,57 +76,27 @@ namespace Hypepool.Monero.Tests.JobManager
 
             await _poolContext.JobManager.Start(); // start the job manager.
 
-            //_poolContext.JobManager.JobQueue
-            //    .Take(3)
-            //    .Subscribe(x =>
-            //    {
-            //        Console.WriteLine(x);
-            //    }, () => 
-                    
-            //        Console.WriteLine("Completed"));
+            // check first response.
+            await _jobManager.JobQueue.Take(1).Do(job =>
+            {
+                job.BlockTemplate.Blob.ShouldBeEquivalentTo("07079ccdc1d3053b4aff4fdac0424b58a69234796f1e572eea2650cb4a6c19aa03c17308d73d5b0000000002e3b64201ffa7b64201b3be90f18de9010258f3cdf062d631c578597ad4aa00bb6e99bbbdc816fa295286ea956c2d15e1cf2b0109d8ec8c47fb41668d69ef2ce3764a906285bbab66a127247f627b939380ddc7020800000000000000000000");
+                job.BlockTemplate.Difficulty.ShouldBeEquivalentTo(23096);
+                job.BlockTemplate.Height.ShouldBeEquivalentTo(1088295);
+                job.BlockTemplate.PreviousBlockhash.ShouldBeEquivalentTo("3b4aff4fdac0424b58a69234796f1e572eea2650cb4a6c19aa03c17308d73d5b");
+                job.BlockTemplate.ReservedOffset.ShouldBeEquivalentTo(129);
+                job.BlockTemplate.Status.ShouldBeEquivalentTo("OK");
+            });
 
-
-            //var test2 = _poolContext.JobManager.JobQueue.Take(2)
-            //    .Subscribe(x =>
-            //    {
-            //        var b = "b";
-            //    });
-
-            //var a = "b";
-
-
-            //_poolContext.JobManager.JobQueue.Wait(); // wait for rx to process.
-
-            //var task = Task.Factory.StartNew(() =>
-            //{
-            //    _poolContext.JobManager.JobQueue.Subscribe(_ =>
-            //    {
-            //        var response = ((MoneroJobManager) _poolContext.JobManager).CurrentJob;
-
-            //        if (counter == 1) // check first response.
-            //        {
-            //            response.BlockTemplate.Blob.ShouldBeEquivalentTo("07079ccdc1d3053b4aff4fdac0424b58a69234796f1e572eea2650cb4a6c19aa03c17308d73d5b0000000002e3b64201ffa7b64201b3be90f18de9010258f3cdf062d631c578597ad4aa00bb6e99bbbdc816fa295286ea956c2d15e1cf2b0109d8ec8c47fb41668d69ef2ce3764a906285bbab66a127247f627b939380ddc7020800000000000000000000");
-            //            response.BlockTemplate.Difficulty.ShouldBeEquivalentTo(23096);
-            //            response.BlockTemplate.Height.ShouldBeEquivalentTo(1088295);
-            //            response.BlockTemplate.PreviousBlockhash.ShouldBeEquivalentTo("3b4aff4fdac0424b58a69234796f1e572eea2650cb4a6c19aa03c17308d73d5b");
-            //            response.BlockTemplate.ReservedOffset.ShouldBeEquivalentTo(129);
-            //            response.BlockTemplate.Status.ShouldBeEquivalentTo("OK");
-            //        }
-            //        else if (counter == 2) // check second response.
-            //        {
-            //            response.BlockTemplate.Blob.ShouldBeEquivalentTo("0707b0cfc1d305a6a0d38480cde292cbcd372835fb8d57ff8285b5334c724f610599d379c5499d0000000002e4b64201ffa8b64201ee86ece98de90102d287d0af34a8c9dc624cf4a7495f673782d9a323b1d22dec71b069af84c072672b013b54f7d869b695fd5e5fa05ddf49e971f511440b86ff42ce836283d207777442020800000000000000000000");
-            //            response.BlockTemplate.Difficulty.ShouldBeEquivalentTo(23087);
-            //            response.BlockTemplate.Height.ShouldBeEquivalentTo(1088296);
-            //            response.BlockTemplate.PreviousBlockhash.ShouldBeEquivalentTo("a6a0d38480cde292cbcd372835fb8d57ff8285b5334c724f610599d379c5499d");
-            //            response.BlockTemplate.ReservedOffset.ShouldBeEquivalentTo(129);
-            //            response.BlockTemplate.Status.ShouldBeEquivalentTo("OK");
-            //        }
-            //    });
-
-            //    _poolContext.JobManager.JobQueue.Wait(); // wait for rx to process.
-            //});
-
-            //task.Wait(1000);
+            // check the second response
+            await _jobManager.JobQueue.Take(1).Do(job =>
+            {
+                job.BlockTemplate.Blob.ShouldBeEquivalentTo("0707b0cfc1d305a6a0d38480cde292cbcd372835fb8d57ff8285b5334c724f610599d379c5499d0000000002e4b64201ffa8b64201ee86ece98de90102d287d0af34a8c9dc624cf4a7495f673782d9a323b1d22dec71b069af84c072672b013b54f7d869b695fd5e5fa05ddf49e971f511440b86ff42ce836283d207777442020800000000000000000000");
+                job.BlockTemplate.Difficulty.ShouldBeEquivalentTo(23087);
+                job.BlockTemplate.Height.ShouldBeEquivalentTo(1088296);
+                job.BlockTemplate.PreviousBlockhash.ShouldBeEquivalentTo("a6a0d38480cde292cbcd372835fb8d57ff8285b5334c724f610599d379c5499d");
+                job.BlockTemplate.ReservedOffset.ShouldBeEquivalentTo(129);
+                job.BlockTemplate.Status.ShouldBeEquivalentTo("OK");
+            });
         }
 
         [Fact]

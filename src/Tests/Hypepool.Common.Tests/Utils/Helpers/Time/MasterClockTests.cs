21b74e276e13d1158fee238726d72a6e71c449d8
huseyin uslu <shalafiraistlin@gmail.com>
2018-02-01 18:03:04 +0300

    fix unsafe code option for release mode. adds bencmarkdotnet support

---
+++ src/Tests/Hypepool.Common.Tests/Utils/Helpers/Time/MasterClockTests.cs
@@ -0,0 +1,63 @@
+﻿#region license
+// 
+//      hypepool
+//      https://github.com/bonesoul/hypepool
+// 
+//      Copyright (c) 2013 - 2018 Hüseyin Uslu
+// 
+//      Permission is hereby granted, free of charge, to any person obtaining a copy
+//      of this software and associated documentation files (the "Software"), to deal
+//      in the Software without restriction, including without limitation the rights
+//      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//      copies of the Software, and to permit persons to whom the Software is
+//      furnished to do so, subject to the following conditions:
+// 
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//      SOFTWARE.
+#endregion
+
+using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using Xunit;
+
+namespace Hypepool.Common.Tests.Utils.Helpers.Time
+{
+    public class MasterClockTests
+    {
+        [Fact]
+        public void TestMasterClockPerformance()
+        {
+
+        }
+    }
+
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+}
